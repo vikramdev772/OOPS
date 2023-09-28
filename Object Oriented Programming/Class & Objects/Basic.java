0f78c4a60d0ae618644118488501fb87cca3f591
vikram <vikramdev772@gmail.com>
2023-09-28 11:44:51 +0530

    Basic

---
+++ Object Oriented Programming/Class & Objects/Basic.java
@@ -0,0 +1,20 @@
+
+public class Basic {
+    public static void main(String[] args) {
+        System.out.println("\n\t Object Oriented Programming \n");
+        System.out.println(
+                "\n\t Oops is core of java programming,\n\t which is used for designing a program that uses\n\t\tObjects & Classes ");
+        System.out.println("\n\t it promotes Modularity , Reusability , Maintainability of code \n");       
+        System.out.println("\n\t Class is a blueprint which defines some properties and behaviors \n"); 
+        System.out.println("\n\t Object are real entities.object is an instance of class. \n"); 
+        Animal human = new Animal();//Animal is a class
+        human.eat();//human is a object eat is a method
+    }
+}
+
+class Animal{
+
+    private static void eat(){
+        System.out.println("\n\t Animal is eating.. \n");
+    }
+}
