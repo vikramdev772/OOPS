6eee36836a0e2e61ec60894ac1004efb2a7dc2ae
vikram <vikramdev772@gmail.com>
2023-09-29 10:15:27 +0530

    Parameterized Constructors

--- Object Oriented Programming/Constructor/Main.java
+++ Object Oriented Programming/Constructor/Main.java
@@ -6,7 +6,9 @@ public class Main {
                 "\n\t it is used to create the instance of the class\n\tConstructor are almost similar to methods (functions )\n\t Expect for two things. \n\t its name is the same as the class Name \n\t * it has no return type .");
         System.out.println("\n\t it refered to as special methods \n");
         Create con = new Create();//Create() constructor is called .
-        Types non = new Types();
+        Types non = new Types(); //Non Parameterized
+        Params p = new Params(5, 6);//Parameterized
+        
 
 
     }
---
+++ Object Oriented Programming/Constructor/Params.java
@@ -0,0 +1,7 @@
+public class Params {
+    Params(int a,int b){
+        System.out.println("\n\t Parameterized params constructor is called \n");
+        System.out.println("\n\t Sum of nums : "+(-(-a-b)));        
+    }
+    
+}
