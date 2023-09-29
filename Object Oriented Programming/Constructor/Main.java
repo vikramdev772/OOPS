baa73a368ef644c1b52a50978bd37330d5e289c2
vikram <vikramdev772@gmail.com>
2023-09-29 10:06:08 +0530

    Constructor

---
+++ Object Oriented Programming/Constructor/Main.java
@@ -0,0 +1,20 @@
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("\n\t Constructor in java \n");
+        System.out.println(
+                "\n\t it is used to create the instance of the class\n\tConstructor are almost similar to methods (functions )\n\t Expect for two things. \n\t its name is the same as the class Name \n\t * it has no return type .");
+        System.out.println("\n\t it refered to as special methods \n");
+        Create con = new Create();
+
+
+    }
+}
+
+class Create {
+    Create() {
+
+        System.out.println("\n\t Create constructor is called \n");
+    }
+
+}
