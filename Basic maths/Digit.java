e4a816bb450900baf8b72b9860546af65f43a0b7
vikram <vikramdev772@gmail.com>
2023-09-30 10:29:05 +0530

    Count

---
+++ Basic maths/Digit.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Digit{
+    public static void main(String[] args) {
+        System.out.println("\n\t Digit Concept \n");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("\n\t Enter a num : ");
+        int n=sc.nextInt();
+        System.out.println("\n\t Number : "+n);
+        count(n);
+        
+    }
+    private static int count(int n){
+        int c=0;
+        while(n>0){
+            int lastDigit = n%10;
+            c=c+1;
+            n=n/10;
+        }
+     System.out.println("\n\t number of digits present in a number : "+c);
+        return c;
+
+    }
+}
