5eeb436c567ffe05439ca618eb293b148ff7be30
vikram <vikramdev772@gmail.com>
2023-10-01 20:13:45 +0530

    Prime check

---
+++ Basic maths/Prime.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Prime {
+    public static void main(String[] args) {
+        System.out.println("\n\t Prime check \n");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("\n\t Enter a num : ");
+        int n=sc.nextInt();
+
+        int count =0;
+        for(int i=1;i*i<=n;i++){
+            if(n%i==0){
+                count++;
+                if((n/i)!=i){
+                    count++;
+                }
+            }
+        }if(count==2) {
+            System.out.println("\n\t it is prime \n");
+        }else{
+            System.out.println("\n\t it not a prime\n");
+        }
+    }
+}
