20b635e5876240b900eb4f1e3896e79a1e8060ba
vikram <vikramdev772@gmail.com>
2023-10-08 16:48:14 +0530

    Alpha pattern

---
+++ Patterns/Print.java
@@ -0,0 +1,37 @@
+// public class Print{
+//     public static void main(String[] args) {
+//         System.out.println("\n\t Patterns \n");
+//         int n = 6;
+//         Alpha(n);
+
+//     }
+//      private static void Alpha(int  n)}{
+//         System.out.println("\n\t Alpha triangle \n");
+//         for(int i=0;i<=n;i++){
+//             char ch='A'+i;
+//             for(int j=0;j<i;j++){
+//                 System.out.print("\t"+ch);
+//             }
+//             System.out.print("\n");
+//         }
+//     }
+// }
+
+public class Print {
+    public static void main(String[] args) {
+        System.out.println("\n\t Patterns \n");
+        int n = 6;
+        Alpha(n);
+    }
+
+    private static void Alpha(int n) {
+        System.out.println("\n\t Alpha triangle \n");
+        for (int i = 1; i <=n; i++) {
+            char ch = (char) ('A' + i);
+            for (int j = 0; j < i; j++) {
+                System.out.print("\t" + ch);
+            }
+            System.out.print("\n");
+        }
+    }
+}
