f69e1312b6466ff349f33ee2f2b07125b35ebf8a
vikram <vikramdev772@gmail.com>
2023-10-13 21:00:03 +0530

    Triangle pattern

---
+++ Patterns/Triangle.java
@@ -0,0 +1,27 @@
+
+public class Triangle{
+    public static void main(String[] args) {
+        System.out.println("\n\t Triangle pattern \n");
+        int n=6;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<=i;j++){
+                System.out.print("\t*");
+            }
+            System.out.println("\n");
+        }
+    }
+}
+
+                Triangle pattern 
+
+        *
+
+        *       *
+
+        *       *       *
+
+        *       *       *       *
+
+        *       *       *       *       *
+
+        *       *       *       *       *       *
