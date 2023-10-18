3b2100ea1c22d8bbd309c91476bdb410403f1e74
vikram <vikramdev772@gmail.com>
2023-10-18 19:20:51 +0530

    Binary square

---
+++ Patterns/Binary.java
@@ -0,0 +1,35 @@
+public class Binary {
+    public static void main(String[] args) {
+        System.out.println("\n\t Binary Square \n");
+        int i,j;
+        int n=5;
+        for(i=1;i<=n;i++){
+            for(j=1;j<=n;j++){
+                System.out.print("\t "+j%2+" ");
+            }
+            System.out.println("\n");
+        }
+
+    }
+    
+}
+                        output:
+
+
+                        Binary Square
+
+         1       0       1       0       1 
+
+         1       0       1       0       1
+
+         1       0       1       0       1
+
+         1       0       1       0       1
+
+         1       0       1       0       1
+
+
+
+
+
+         
--- Patterns/Inverted.java
+++ Patterns/Inverted.java
@@ -10,3 +10,15 @@ public class Inverted {
         }
     }
 }
+                    Inverted Triangle
+
+         7
+         7       6
+         7       6       5
+         7       6       5       4
+         7       6       5       4       3
+         7       6       5       4       3       2
+         7       6       5       4       3       2       1
+
+
+         
